20437db2cc5edec06a70c1541619edbab336edb7
Martin Krämer <mkraemer.de@gmail.com>
2015-02-12 01:04:11 +0100

    0.2.3: remove numpy related deps from setup.py till finding better solution

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="plugml",
-    version="0.2.2",
+    version="0.2.3",
     description="easy-to-use and highly modular machine learning framework",
     long_description="easy-to-use and highly modular machine learning framework based on scikit-learn with postgresql data bindings",
     url="https://github.com/mkraemer67/plugml",
@@ -13,11 +13,7 @@ setup(
     license="Apache",
     packages=["plugml"],
     install_requires=[
-        "nltk>=3.0.0",
-        "numpy>=1.9.0",
         "psycopg2>=2.5.0",
-        "scikit-learn>=0.15.0",
-        "scipy>=0.14.0"
     ],
     include_package_data=True,
     zip_safe=False
